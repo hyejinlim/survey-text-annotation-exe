251aa8a15c2ddbd338e5ceb7fc784f3c5e8b613e
hyejinlim <yoon1440_@naver.com>
2024-11-11 18:25:12 +0900

    ✨ JSON 다운로드

--- 12-1/src/components/annotation/survey/ToolHeader/ToolHeader.tsx
+++ 12-1/src/components/annotation/survey/ToolHeader/ToolHeader.tsx
@@ -1,6 +1,7 @@
 import { memo, useContext } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { Button } from 'reactstrap';
+import Swal from 'sweetalert2';
 import { SurveyTextAnnotationContext } from '~/libs/contexts/SurveyTextAnnotationContext';
 import * as styles from './styles';
 
@@ -9,9 +10,34 @@ function ToolHeader() {
   const { document, exeLabelingList } = useContext(SurveyTextAnnotationContext);
   const { info } = document;
 
+  const getSurveyAChoicesArray = (data: any) => {
+    return Object.keys(data)
+      .reduce((acc: any, cur: any) => {
+        if (/surveyAChoices/.exec(cur)) return [...acc, data[cur]];
+        return acc;
+      }, [])
+      .filter((el: any) => el);
+  };
+
+  const createJsonDownloadLink = ({ data, fileName, fileType }: any) => {
+    const blob = new Blob([data], { type: fileType });
+    const a = window.document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(a.href);
+  };
+
   const handleClick = (values: any) => {
-    console.log('values', values);
-    console.log('exeLabelingList', exeLabelingList);
+    if (exeLabelingList?.length === 0) {
+      Swal.fire({
+        icon: 'error',
+        title: '문항별 정보를 작성해주세요.',
+        confirmButtonText: '확인',
+      });
+      return;
+    }
+
     const {
       surveyPurpose,
       surveyMethod,
@@ -27,32 +53,97 @@ function ToolHeader() {
       surveyAge,
       surveyLocation,
     } = values;
-    const jsonData = {
-      survey_info: {
-        id: '',
-        survey_purpose: surveyPurpose,
-        method: surveyMethod?.label,
-        industry: surveyIndustry?.label,
-        topic: {
-          topic: surveyTopic?.label,
-          topic_detailed: surveyTopicDetail?.label,
-        },
-        keyword: surveyKeyword,
-        q_result: {
-          calculation: surveyCalculation,
-          cutoff: surveyCutoff,
-          credibility: surveyCredibility,
-          validity: surveyValidity,
-        },
-        target_info: {
-          gender: surveyGender?.label,
-          age: surveyAge?.map(({ label }: any) => label),
-          location: surveyLocation,
-        },
+
+    const surveyJsonData = {
+      id: '',
+      survey_purpose: surveyPurpose,
+      method: surveyMethod?.label,
+      industry: surveyIndustry?.label,
+      topic: {
+        topic: surveyTopic?.label,
+        topic_detailed: surveyTopicDetail?.label,
+      },
+      keyword: surveyKeyword,
+      q_result: {
+        calculation: surveyCalculation,
+        cutoff: surveyCutoff,
+        credibility: surveyCredibility,
+        validity: surveyValidity,
+      },
+      target_info: {
+        gender: surveyGender?.label,
+        age: surveyAge?.map(({ label }: any) => label),
+        location: surveyLocation,
       },
     };
 
-    console.log('jsonData', jsonData);
+    exeLabelingList.forEach((data: any) => {
+      const {
+        surveyQNum,
+        surveyContext,
+        surveyQPart,
+        surveyQPurpose,
+        surveyAType,
+      } = data;
+
+      const surveyAChoicesArray = getSurveyAChoicesArray(data);
+
+      const surveyLogicArray = Object.keys(data)
+        .reduce((acc: any, cur: any) => {
+          const [key, index] = cur.split('_');
+          if (/surveyNextQNum/.exec(key) || /surveyNextQCondition/.exec(key)) {
+            const newData = { ...acc[index], [key]: data[cur] };
+            acc[index] = newData;
+            return acc;
+          }
+          return acc;
+        }, [])
+        .filter((el: any) => el);
+
+      const logicObj = surveyLogicArray.map((data: any) => {
+        const { surveyNextQNum, surveyNextQCondition } = data;
+        const filtered = exeLabelingList.filter(
+          ({ surveyQNum }: any) => surveyQNum === surveyNextQNum
+        )[0];
+        const filteredSurveyAChoicesArray = getSurveyAChoicesArray(filtered);
+
+        return {
+          next_condition: surveyNextQCondition,
+          next_q_num: surveyNextQNum,
+          next_q_context: filtered?.surveyContext,
+          next_answer: {
+            next_type: filtered?.surveyAType?.label,
+            next_num: filteredSurveyAChoicesArray?.length,
+            next_choices: filteredSurveyAChoicesArray,
+          },
+        };
+      });
+
+      const questionJsonData = {
+        q_num: surveyQNum,
+        context: surveyContext,
+        q_part: surveyQPart,
+        q_purpose: surveyQPurpose,
+        answer: {
+          type: surveyAType?.label,
+          num: surveyAChoicesArray?.length,
+          choices: surveyAChoicesArray,
+        },
+        logic: logicObj,
+      };
+
+      const jsonData = {
+        survey_info: surveyJsonData,
+        question_info: questionJsonData,
+      };
+
+      const name = `label_${surveyQNum}`; //파일명
+      createJsonDownloadLink({
+        data: JSON.stringify(jsonData),
+        fileName: `${name}.json`,
+        fileType: 'text/json',
+      });
+    });
   };
 
   return (
